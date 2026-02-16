e88b9916ffeab553a8aa6e269639fb56a3e66c31
paulo <phaleixo1@gmail.com>
2026-02-16 11:59:38 -0300

    modified:   src/app/curriculo/page.tsx

--- src/app/curriculo/page.tsx
+++ src/app/curriculo/page.tsx
@@ -37,18 +37,27 @@ export default function CurriculoPage() {
           </div>
         </div>
 
-        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-200 pl-3 uppercase mt-6">
+        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-900 pl-3 uppercase mt-6">
           Resumo Profissional
         </h3>
         <div className="mb-5 pr-5">
           Desenvolvedor Web Fullstack com 3 anos de experiência, especializado
-          no ecossistema JavaScript e tecnologias modernas de nuvem. Expertise
-          em todo o ciclo de vida de software, desde a concepção de protótipos e
-          design UX/UI até o deploy e manutenção. Focado em Clean Code,
-          performance e escalabilidade.
+          no ecossistema JavaScript e tecnologias modernas de nuvem.
+          <br />
+          Experiencia com NextJS em frontend/backend e criação de APIs modernas.
+          <br />
+          Conhecimento avançado de 10 anos em Hardware e Software, com foco em
+          servidores Linux, automação de deploy e manutenção de sistemas.
+          <br />
+          Habilidade em diagnóstico e resolução de problemas complexos,
+          garantindo alta disponibilidade e desempenho.
+          <br />
+          Expertise em todo o ciclo de vida de software, desde a concepção de
+          protótipos e design UX/UI até o deploy e manutenção. <br />
+          Focado em Clean Code, performance e escalabilidade.
         </div>
 
-        <h3 className="text-sm md:text-base font-semibold text-gray-800 border-l-4 border-gray-200 pl-3 uppercase mt-6">
+        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-900 pl-3 uppercase mt-6">
           Tecnologias e Ferramentas
         </h3>
         <div className="mb-5 pr-5">
@@ -91,8 +100,34 @@ export default function CurriculoPage() {
             </span>
           </div>
         </div>
+        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
+          Formação Acadêmica
+        </h3>
+        <div className="mb-5 pr-5">
+          <ul className="list-disc pl-5 space-y-2">
+            <li>
+              <strong>Pós-Graduação: Desenvolvimento Web Fullstack</strong> -
+              IFSULDEMINAS (2026)
+            </li>
+            <li>
+              <strong>Bacharelado em Tecnologia da Informação</strong> - UNIVESP
+              (2025)
+            </li>
+            <li>
+              <strong>Bootcamps:</strong> Java Backend e Ciência de Dados
+              (Santander), Banco de Dados (IFSul).
+            </li>
+          </ul>
+        </div>
 
-        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-200 pl-3 uppercase mt-6">
+        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
+          Idiomas
+        </h3>
+        <div className="mb-5 pr-5">
+          <strong>Português:</strong> Nativo | <strong>Inglês:</strong> Básico
+          (Leitura técnica e documentação)
+        </div>
+        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-900 pl-3 uppercase mt-6">
           Projetos em Destaque
         </h3>
         <div className="mb-5 pr-5">
@@ -107,6 +142,10 @@ export default function CurriculoPage() {
               >
                 www.taynarathompson.com.br
               </a>
+              <p>
+                Landing page para escritora e comunicadora ,em NextJs com codigo
+                limpo e otimizado para SEO com metadata.
+              </p>
             </li>
             <li>
               <strong>EcoSol - Economia Solidária:</strong>{" "}
@@ -118,6 +157,10 @@ export default function CurriculoPage() {
               >
                 ecosolautista.com.br
               </a>
+              <p>
+                Site para projeto de economia solidária com design responsivo e
+                otimizado para SEO.
+              </p>
             </li>
             <li>
               <strong>Mônica Menezes:</strong>{" "}
@@ -129,10 +172,10 @@ export default function CurriculoPage() {
               >
                 monicamenezes.shop
               </a>
+              <p>Loja de produtos de beleza e cosmeticos de qualidade.</p>
             </li>
             <li>
-              <strong>API de Clima -Proxy e cache de dados
-              meteorológicos via REDEMET API:</strong>{" "}
+              <strong>API de Clima:</strong>{" "}
               <a
                 className="text-gray-800 underline"
                 href="https://weather-api-dun-mu.vercel.app/"
@@ -141,31 +184,38 @@ export default function CurriculoPage() {
               >
                 https://weather-api-dun-mu.vercel.app/
               </a>
+              <p>Proxy e cache de dados meteorológicos via REDEMET API</p>
             </li>
             <li>
-              <strong>Servidores RP - Acesso rápido aos sistemas de RH para servidores de Ribeirão Preto.
-:</strong> {" "}
+              <strong>Servidores RP:</strong>{" "}
               <a
                 className="text-gray-800 underline"
                 href="https://phaleixo.github.io/Servidores_RP/index"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                https://phaleixo.github.io/Servidores_RP/index  
+                https://phaleixo.github.io/Servidores_RP/index
               </a>
+              <p>
+                Aplicação web para acesso rápido aos sistemas de RH para
+                servidores de Ribeirão Preto, com interface simples e funcional.
+              </p>
             </li>
             <li>
-              <strong>DiverGente - Aplicativo para dispositivos moveis com rotina e gestao de tarefas.
-:</strong> {" "}
+              <strong>DiverGente :</strong>{" "}
               <a
                 className="text-gray-800 underline"
                 href="https://phaleixo.github.io/DiverGente/index"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                https://phaleixo.github.io/DiverGente/index  
+                https://phaleixo.github.io/DiverGente/index
               </a>
             </li>
+            <p>
+              Aplicativo para dispositivos moveis com rotina e gestao de
+              tarefas.
+            </p>
           </ul>
         </div>
 
@@ -202,34 +252,6 @@ export default function CurriculoPage() {
               </li>
             </ul>
           </div>
-        </div>
-
-        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
-          Formação Acadêmica
-        </h3>
-        <div className="mb-5 pr-5">
-          <ul className="list-disc pl-5 space-y-2">
-            <li>
-              <strong>Pós-Graduação: Desenvolvimento Web Fullstack</strong> -
-              IFSULDEMINAS (2026)
-            </li>
-            <li>
-              <strong>Bacharelado em Tecnologia da Informação</strong> - UNIVESP
-              (2025)
-            </li>
-            <li>
-              <strong>Bootcamps:</strong> Java Backend e Ciência de Dados
-              (Santander), Banco de Dados (IFSul).
-            </li>
-          </ul>
-        </div>
-
-        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
-          Idiomas
-        </h3>
-        <div className="mb-5 pr-5">
-          <strong>Português:</strong> Nativo | <strong>Inglês:</strong> Básico
-          (Leitura técnica e documentação)
         </div>
       </div>
     </main>
