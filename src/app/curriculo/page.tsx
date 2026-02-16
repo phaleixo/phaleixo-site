5b47415964e86afa87daa0a972ba68c1272ee9a7
paulo <phaleixo1@gmail.com>
2026-02-16 11:40:20 -0300

    new file:   src/app/curriculo/page.tsx

---
+++ src/app/curriculo/page.tsx
@@ -0,0 +1,237 @@
+import React from "react";
+
+export default function CurriculoPage() {
+  return (
+    <main>
+      <div className="p-10 bg-white text-gray-800 font-sans">
+        <div className="text-center border-b-2 border-gray-200 pb-5">
+          <h1 className="text-3xl md:text-4xl font-extrabold text-slate-900 mb-2">
+            Paulo Henrique Aleixo de Campos
+          </h1>
+          <div className="text-sm text-gray-800 mt-2">
+            Jardinópolis, SP | (16) 988380508 |{" "}
+            <a
+              className="text-gray-800 underline"
+              href="mailto:phaleixo@outlook.com.br"
+            >
+              phaleixo@outlook.com.br
+            </a>
+            <br />
+            <a
+              className="text-gray-800 underline"
+              href="https://phaleixo.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              phaleixo.dev
+            </a>{" "}
+            |{" "}
+            <a
+              className="text-gray-800 underline"
+              href="https://github.com/phaleixo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github.com/phaleixo
+            </a>
+          </div>
+        </div>
+
+        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-200 pl-3 uppercase mt-6">
+          Resumo Profissional
+        </h3>
+        <div className="mb-5 pr-5">
+          Desenvolvedor Web Fullstack com 3 anos de experiência, especializado
+          no ecossistema JavaScript e tecnologias modernas de nuvem. Expertise
+          em todo o ciclo de vida de software, desde a concepção de protótipos e
+          design UX/UI até o deploy e manutenção. Focado em Clean Code,
+          performance e escalabilidade.
+        </div>
+
+        <h3 className="text-sm md:text-base font-semibold text-gray-800 border-l-4 border-gray-200 pl-3 uppercase mt-6">
+          Tecnologias e Ferramentas
+        </h3>
+        <div className="mb-5 pr-5">
+          <div className="flex flex-wrap gap-2">
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Next.js
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              React
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Node.js
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Tailwind CSS
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Docker
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Python
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Java
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              SQL & NoSQL
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Vercel
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Ubuntu Server
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Expo (Mobile)
+            </span>
+            <span className="bg-gray-100 border border-gray-200 px-2 py-1 rounded text-sm text-gray-800">
+              Git/GitHub
+            </span>
+          </div>
+        </div>
+
+        <h3 className="text-sm md:text-base font-semibold text-gray-900 border-l-4 border-gray-200 pl-3 uppercase mt-6">
+          Projetos em Destaque
+        </h3>
+        <div className="mb-5 pr-5">
+          <ul className="list-disc pl-5 space-y-2">
+            <li>
+              <strong>Taynara Thompson:</strong>{" "}
+              <a
+                className="text-gray-800 underline"
+                href="https://www.taynarathompson.com.br"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                www.taynarathompson.com.br
+              </a>
+            </li>
+            <li>
+              <strong>EcoSol - Economia Solidária:</strong>{" "}
+              <a
+                className="text-gray-800 underline"
+                href="https://ecosolautista.com.br"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ecosolautista.com.br
+              </a>
+            </li>
+            <li>
+              <strong>Mônica Menezes:</strong>{" "}
+              <a
+                className="text-gray-800 underline"
+                href="https://monicamenezes.shop"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                monicamenezes.shop
+              </a>
+            </li>
+            <li>
+              <strong>API de Clima -Proxy e cache de dados
+              meteorológicos via REDEMET API:</strong>{" "}
+              <a
+                className="text-gray-800 underline"
+                href="https://weather-api-dun-mu.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://weather-api-dun-mu.vercel.app/
+              </a>
+            </li>
+            <li>
+              <strong>Servidores RP - Acesso rápido aos sistemas de RH para servidores de Ribeirão Preto.
+:</strong> {" "}
+              <a
+                className="text-gray-800 underline"
+                href="https://phaleixo.github.io/Servidores_RP/index"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://phaleixo.github.io/Servidores_RP/index  
+              </a>
+            </li>
+            <li>
+              <strong>DiverGente - Aplicativo para dispositivos moveis com rotina e gestao de tarefas.
+:</strong> {" "}
+              <a
+                className="text-gray-800 underline"
+                href="https://phaleixo.github.io/DiverGente/index"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://phaleixo.github.io/DiverGente/index  
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
+          Experiência Profissional
+        </h3>
+        <div className="mb-5 pr-5 space-y-4">
+          <div>
+            <div className="flex justify-between font-semibold">
+              <span>Desenvolvedor Web (Freelancer)</span>
+              <span>Jan 2023 – Atual</span>
+            </div>
+            <ul className="list-disc pl-5 mt-2">
+              <li>
+                Desenvolvimento Fullstack de aplicações robustas com foco em
+                experiência do usuário.
+              </li>
+              <li>
+                Gestão de infraestrutura em servidores Linux e automação de
+                deploy com Docker e Vercel.
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <div className="flex justify-between font-semibold">
+              <span>Técnico em Manutenção de Computadores | TechNet</span>
+              <span>Out 2013 – Dez 2022</span>
+            </div>
+            <ul className="list-disc pl-5 mt-2">
+              <li>Diagnóstico avançado de hardware e sistemas operacionais.</li>
+              <li>
+                Planejamento de manutenções preventivas e documentação técnica.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
+          Formação Acadêmica
+        </h3>
+        <div className="mb-5 pr-5">
+          <ul className="list-disc pl-5 space-y-2">
+            <li>
+              <strong>Pós-Graduação: Desenvolvimento Web Fullstack</strong> -
+              IFSULDEMINAS (2026)
+            </li>
+            <li>
+              <strong>Bacharelado em Tecnologia da Informação</strong> - UNIVESP
+              (2025)
+            </li>
+            <li>
+              <strong>Bootcamps:</strong> Java Backend e Ciência de Dados
+              (Santander), Banco de Dados (IFSul).
+            </li>
+          </ul>
+        </div>
+
+        <h3 className="text-sm md:text-base font-semibold  text-gray-900 border-l-4  border-gray-900 pl-3 uppercase mt-6">
+          Idiomas
+        </h3>
+        <div className="mb-5 pr-5">
+          <strong>Português:</strong> Nativo | <strong>Inglês:</strong> Básico
+          (Leitura técnica e documentação)
+        </div>
+      </div>
+    </main>
+  );
+}
